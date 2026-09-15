4d6cc7903ae33dc4bee9c2e130cdd088f5500eb6
hixianfu <2357823678@qq.com>
2026-09-15 20:12:14 +0800

    feat: relate articles to faqs

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -526,6 +526,7 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
       Schema.Attribute.SetMinMaxLength<{
         maxLength: 80;
       }>;
+    faqs: Schema.Attribute.Relation<'manyToMany', 'api::faq.faq'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
